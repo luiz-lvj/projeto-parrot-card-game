d9e80a080498c234feb674fb36de07107cf331bd
Luiz <lvj253@gmail.com>
2021-04-09 16:20:30 -0300

    adicionando cards dinamicamente

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
-function dealCards(){
-    let number_cards = parseInt(prompt("Com quantas cartas você quer jogar?"));
-    while(!(typeof(number_cards)== "number" && number_cards >= 4 && number_cards <= 14 && number_cards % 2 === 0)){
-        number_cards = parseInt(prompt("Entrada Inválida\nA quantidade de cartas é um número par entre 4 e 14.", "Digite a quantidade de cartas..."));
-    }
-    console.log(number_cards)
-}
 dealCards();
+function dealCards(){
+    let numberCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
+    while(!(typeof(numberCards)== "number" && numberCards >= 4 && numberCards <= 14 && numberCards % 2 === 0)){
+        numberCards = parseInt(prompt("Entrada Inválida\nA quantidade de cartas é um número par entre 4 e 14.", "Digite a quantidade de cartas..."));
+    }
+
+    lineCards = document.querySelector(".card-line1");
+    for(let i = 0; i< numberCards; ++i){
+        lineCards.innerHTML += createCard();
+    }
+}
+function createCard(){
+    let cardLine = `<div class="card" onClick="selectCard(this)">
+                            <div class="card-game">
+                                <img src="img/front.png" class="img-front-card">
+                            </div>
+                            <div class="card-game back">
+                                <h1>:)</h1>
+                            </div>
+                        </div>`;
+    return cardLine;
+}
 
 function selectCard(obj){
-    let obj_c = obj.children;
-    console.log(obj_c);
-    obj_c[0].classList.add("front");
-    obj_c[1].classList.add("back-onClick");
-    if(obj_c[1].classList.contains("back")){
-        obj_c[1].classList.remove("back");
+    let objChildren = obj.children;
+    console.log(objChildren);
+    objChildren[0].classList.add("front");
+    objChildren[1].classList.add("back-onClick");
+    if(objChildren[1].classList.contains("back")){
+        objChildren[1].classList.remove("back");
     }
-    setTimeout(()=>{unselectCard(obj_c)}, 2000);
-    console.log(obj_c[0]);
-    console.log(obj_c[1]);
+    setTimeout(()=>{unselectCard(objChildren)}, 2000);
+    console.log(objChildren[0]);
+    console.log(objChildren[1]);
 }
 
-function unselectCard(obj_c){
-    if(obj_c[0].classList.contains("front")){
-        obj_c[0].classList.remove("front");
+function unselectCard(objChildren){
+    if(objChildren[0].classList.contains("front")){
+        objChildren[0].classList.remove("front");
     }
-    if(obj_c[1].classList.contains("back-onClick")){
-        obj_c[1].classList.remove("back-onClick");
+    if(objChildren[1].classList.contains("back-onClick")){
+        objChildren[1].classList.remove("back-onClick");
     }
-    obj_c[1].classList.add("back");
+    objChildren[1].classList.add("back");
 }
